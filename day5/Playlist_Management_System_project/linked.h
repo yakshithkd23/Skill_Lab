0a674753a2cdf283a50749bd06b639b9cf17c062
Yakshith K D <157264298+yakshithkd23@users.noreply.github.com>
2025-02-21 21:48:20 +0530

    linked.h

---
+++ day5/Playlist_Management_System_project/linked.h
@@ -0,0 +1,12 @@
+#ifndef LINKED_H
+#define LINKED_H
+
+#include "song.h"
+
+// Function Prototypes
+void insertSong(char *title);
+void displayCurrentSong();
+void nextSong();
+void deleteCurrentSong();
+
+#endif
